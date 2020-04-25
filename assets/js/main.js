8c45a5e9376399de857776748832ec1bf10b46f8
mamunhshimul <mamunhshimul@gmail.com>
2020-04-26 01:04:34 +0600

    added mouse move to effect on header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,32 @@
 
 
-window.addEventListener('scroll' function(e){
+var parlx = function(e, target, parent, movement) {
 
-	var scrollled = window.pageYOffset;
 
-	const background = document.querySelector('.background');
+    var relX = e.pageX - offset(parent).left
+    var relY = e.pageY - offset(parent).top
+    var x = (relX - parent.offsetWidth / 2) / parent.offsetWidth * movement
+    var y = (relY - parent.offsetHeight / 2) / parent.offsetHeight * movement
+    
+    target.style.setProperty('--parallax-x', x+"px");
+    target.style.setProperty('--parallax-y', y+"px");
+  }
 
-	background.style.top = -(scrollled * 0.2) + 'px';
+function offset(el) {
+	
+    var rect = el.getBoundingClientRect(),
+
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop
+
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
+
+document.querySelector(".header").addEventListener("mousemove", function(e) {
+    parlx(e, this.querySelector("h1"), this, 80)
+    parlx(e, this.querySelector("p"), this, 60)
+    parlx(e, this.querySelector(".btn-wrapper"), this, 40)
+
+
+
 })
